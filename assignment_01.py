7924ad1dc08663b20d53a2250498880c611ed980
Laxmipbb <126185936+Laxmipbb@users.noreply.github.com>
2023-02-26 15:43:58 +0530

    Add files via upload

---
+++ assignment_01.py
@@ -0,0 +1,327 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 43,
+   "id": "437ae34c-1772-4ac2-91e2-8d88a22730ab",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'Guido van Rossum'"
+      ]
+     },
+     "execution_count": 43,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"Guido van Rossum\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 44,
+   "id": "0aa7d2ee-284d-48d2-b96b-b0c1ed097edc",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'python is a  multi paradigm programming, object oriented programming and structured programming.'"
+      ]
+     },
+     "execution_count": 44,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"python is a  multi paradigm programming, object oriented programming and structured programming.\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 45,
+   "id": "ff813c3f-4fb9-444a-964d-33198f2aab98",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'yes, python is case sensitive'"
+      ]
+     },
+     "execution_count": 45,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"yes, python is case sensitive\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 46,
+   "id": "e34aa4d2-a318-4d51-a892-3ffd5d3d6d77",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'The multi extension for python files are - .py, .pyi'"
+      ]
+     },
+     "execution_count": 46,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"The multi extension for python files are - .py, .pyi\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 47,
+   "id": "2e730e9f-30e1-489a-83e0-166e57118e93",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'Indentiation is used to block of code in python language'"
+      ]
+     },
+     "execution_count": 47,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"Indentiation is used to block of code in python language\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 48,
+   "id": "e618ce1b-74d4-45ad-b6bd-38f5f5c297b4",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'#'"
+      ]
+     },
+     "execution_count": 48,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"#\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 49,
+   "id": "4971cb54-92f1-42dd-bacc-8f01e9295704",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'sys.version can help us to find the version that are currently working on'"
+      ]
+     },
+     "execution_count": 49,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"sys.version can help us to find the version that are currently working on\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 50,
+   "id": "078f8849-ee83-4281-ae33-250ed972ed55",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'Preffered Installer Programming is the stands for PIP'"
+      ]
+     },
+     "execution_count": 50,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"Preffered Installer Programming is the stands for PIP\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 51,
+   "id": "cf8c945b-8641-45c4-a8de-d86e7c504c78",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "' byte(), array(), bool(), callable(),classmethod() is bult-in function in python'"
+      ]
+     },
+     "execution_count": 51,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\" byte(), array(), bool(), callable(),classmethod() is bult-in function in python\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 52,
+   "id": "6184f320-ff78-4c9f-b34d-d17ba7a52c89",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "' 79  is the maximum possible length of an identifier in python'"
+      ]
+     },
+     "execution_count": 52,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\" 79  is the maximum possible length of an identifier in python\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 53,
+   "id": "2048fc54-5102-440f-9788-5ed5ade8c569",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "' data science , scientific and mathematical development, game development ,finance and trading is the benefit of python'"
+      ]
+     },
+     "execution_count": 53,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\" data science , scientific and mathematical development, game development ,finance and trading is the benefit of python\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 54,
+   "id": "3eb3bb17-18fb-427b-8027-88eeb7bc629d",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'Memory management in python involves a private heap containing all python objects and data structures'"
+      ]
+     },
+     "execution_count": 54,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"Memory management in python involves a private heap containing all python objects and data structures\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 55,
+   "id": "22235be3-3be9-4fa9-942a-f09249d2375b",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'The complete of python (1)Right clicking this PC and going to properties. (2) Clicking on the advanced system setting in the menu on the left (3) Clicking on the environment variable button on the bottom right (4) In the system variable section ,selecting the path variable and clicking on edit'"
+      ]
+     },
+     "execution_count": 55,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"The complete of python (1)Right clicking this PC and going to properties. (2) Clicking on the advanced system setting in the menu on the left (3) Clicking on the environment variable button on the bottom right (4) In the system variable section ,selecting the path variable and clicking on edit\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 56,
+   "id": "366d736e-e9a4-4e6e-8afc-d1c998015b6b",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'yes'"
+      ]
+     },
+     "execution_count": 56,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"yes\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "088bfd46-f4d4-4274-b3e5-7b90b32b1e8d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
